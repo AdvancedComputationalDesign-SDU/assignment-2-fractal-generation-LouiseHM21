49d990314f954b5a1af564934091d314c8c1b48c
LouiseHM21 <lomad21@student.sdu.dk>
2024-11-06 19:20:48 +0100

    Base layout
    The structure of the code

--- fractal_generator.py
+++ fractal_generator.py
@@ -6,3 +6,21 @@ Author: Louise Hedegård Madsen
 Description:
 This script generates fractal patterns using recursive functions and geometric transformations.
 """
+
+# Importing libraries and stuff
+import matplotlib.pyplot as plt
+import math
+
+# Base case
+def base_case (depth):
+    return depth <= 0
+
+# drawing branch
+
+# Recursive case
+
+# plotting the fractal tree
+
+# call the recursive function
+
+plt.show()
